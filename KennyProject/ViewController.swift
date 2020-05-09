304bca24b39b5c556348f49c2a2f339ebb7b8d64
TANRIKUT NEMUTLU <tnemutlu@gmail.com>
2020-05-09 19:28:10 +0300

    Initial Commit

---
+++ KennyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KennyProject
+//
+//  Created by TANRIKUT NEMUTLU on 9.05.2020.
+//  Copyright © 2020 TANRIKUT NEMUTLU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
